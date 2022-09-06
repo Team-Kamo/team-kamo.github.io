23d747e382e3c0d9f96ea1cd715c33bbb6bc6006
h2yk <me@h2yk.dev>
2022-09-06 20:58:10 +0900

    Init landing page

---
+++ src/App.tsx
@@ -0,0 +1,299 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import IconButton from "@mui/material/IconButton";
+import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Button, Card, CardContent, CardHeader, Link } from "@mui/material";
+import { Code, FileDownload, GetApp, NavigateNext } from "@mui/icons-material";
+import Icon from "@mdi/react";
+import { mdiMicrosoftWindows, mdiPenguin, mdiApple, mdiGit } from "@mdi/js";
+
+const darkTheme = createTheme({
+    typography: {
+        button: {
+            textTransform: "none",
+        },
+    },
+    palette: {
+        mode: "dark",
+    },
+});
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <div className="App">
+                <Box sx={{ flexGrow: 1 }}>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Typography variant="h5" sx={{ flexGrow: 1 }}>
+                                Team-Kamo
+                            </Typography>
+                            <Link href="https://github.com/Team-Kamo">
+                                <IconButton
+                                    size="large"
+                                    aria-label="github link"
+                                    aria-controls="menu-appbar"
+                                    color="inherit"
+                                >
+                                    <GitHubIcon />
+                                </IconButton>
+                            </Link>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+                <Box sx={{ flexGrow: 1, m: 20 }}>
+                    <Typography variant="h1" gutterBottom align="center">
+                        Octane
+                    </Typography>
+                    <Typography variant="h2" gutterBottom align="center">
+                        The simple, easy virtual clipboard software
+                    </Typography>
+                    <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <Button
+                            variant="contained"
+                            endIcon={<NavigateNext />}
+                            size="large"
+                        >
+                            Visit our presentation slide
+                        </Button>
+                    </Box>
+                </Box>
+                <Box sx={{ flexGrow: 1, m: 10 }}>
+                    <Typography variant="h3" gutterBottom align="center">
+                        Downloads
+                    </Typography>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Box>
+                                <Typography variant="h4" gutterBottom>
+                                    Server
+                                </Typography>
+                                <Icon
+                                    path={mdiMicrosoftWindows}
+                                    title="Windows"
+                                    size={1}
+                                />
+                                <Icon
+                                    path={mdiPenguin}
+                                    title="Linux"
+                                    size={1}
+                                />
+                                <Icon path={mdiApple} title="Mac" size={1} />
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "right",
+                                }}
+                            >
+                                <a href="https://team-kamo.github.io/server">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={<Code />}
+                                    >
+                                        API document
+                                    </Button>
+                                </a>
+                                <a href="https://github.com/Team-Kamo/server">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={
+                                            <Icon
+                                                path={mdiGit}
+                                                title="Git"
+                                                size={1}
+                                            />
+                                        }
+                                        sx={{ mx: 2 }}
+                                    >
+                                        Visit repository
+                                    </Button>
+                                </a>
+                                <a href="https://github.com/Team-Kamo/server/releases">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={<GetApp />}
+                                    >
+                                        Download release
+                                    </Button>
+                                </a>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Box>
+                                <Typography variant="h4" gutterBottom>
+                                    GUI Client
+                                </Typography>
+                                <Icon
+                                    path={mdiMicrosoftWindows}
+                                    title="Windows"
+                                    size={1}
+                                />
+                                <Icon
+                                    path={mdiPenguin}
+                                    title="Linux"
+                                    size={1}
+                                />
+                                <Icon path={mdiApple} title="Mac" size={1} />
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "right",
+                                }}
+                            >
+                                <a href="https://github.com/Team-Kamo/gui-client">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={
+                                            <Icon
+                                                path={mdiGit}
+                                                title="Git"
+                                                size={1}
+                                            />
+                                        }
+                                        sx={{ mx: 2 }}
+                                    >
+                                        Visit repository
+                                    </Button>
+                                </a>
+                                <a href="https://github.com/Team-Kamo/gui-client/releases">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={<GetApp />}
+                                    >
+                                        Download release
+                                    </Button>
+                                </a>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Box>
+                                <Typography variant="h4" gutterBottom>
+                                    CLI(Python)
+                                </Typography>
+                                <Icon
+                                    path={mdiPenguin}
+                                    title="Linux"
+                                    size={1}
+                                />
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "right",
+                                }}
+                            >
+                                <a href="#">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={
+                                            <Icon
+                                                path={mdiGit}
+                                                title="Git"
+                                                size={1}
+                                            />
+                                        }
+                                        sx={{ mx: 2 }}
+                                        disabled
+                                    >
+                                        Visit repository
+                                    </Button>
+                                </a>
+                                <a href="#">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={<GetApp />}
+                                        disabled
+                                    >
+                                        Download release
+                                    </Button>
+                                </a>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Box>
+                                <Typography variant="h4" gutterBottom>
+                                    API-Client(C++ SDK/Python ffi binding)
+                                </Typography>
+                                <Icon
+                                    path={mdiMicrosoftWindows}
+                                    title="Windows"
+                                    size={1}
+                                />
+                                <Icon
+                                    path={mdiPenguin}
+                                    title="Linux"
+                                    size={1}
+                                />
+                                <Icon path={mdiApple} title="Mac" size={1} />
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "right",
+                                }}
+                            >
+                                <a href="https://team-kamo.github.io/api-client">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={<Code />}
+                                    >
+                                        API document
+                                    </Button>
+                                </a>
+                                <a href="https://github.com/Team-Kamo/api-client">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={
+                                            <Icon
+                                                path={mdiGit}
+                                                title="Git"
+                                                size={1}
+                                            />
+                                        }
+                                        sx={{ mx: 2 }}
+                                    >
+                                        Visit repository
+                                    </Button>
+                                </a>
+                                <a href="https://github.com/Team-Kamo/api-client/releases">
+                                    <Button
+                                        variant="outlined"
+                                        endIcon={<GetApp />}
+                                    >
+                                        Download release
+                                    </Button>
+                                </a>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </Box>
+                <Box sx={{ flexGrow: 1, m: 2 }}>
+                    <Typography variant="body2" gutterBottom align="center">
+                        ©2022 Team-Kamo
+                    </Typography>
+                </Box>
+            </div>
+        </ThemeProvider>
+    );
+}
+
+export default App;
